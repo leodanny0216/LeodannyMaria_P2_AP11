ef8df8c9ddb67badc694485dfeeea7331e4f04c8
leodanny0216 <leodannymaria2@gmail.com>
2025-10-01 22:57:39 -0400

    le agregue el hilt a DeleteHuacalScreen.kt

--- app/src/main/java/edu/ucne/leodannymaria_p2_ap1/presentacion/EntradaHuacal/DeleteHuacalScreen.kt
+++ app/src/main/java/edu/ucne/leodannymaria_p2_ap1/presentacion/EntradaHuacal/DeleteHuacalScreen.kt
@@ -1,13 +1,24 @@
 package edu.ucne.joserivera_ap2_p1.presentation.huacales
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import edu.ucne.huacales.data.local.entities.EntradaHuacalEntity
+import edu.ucne.leodannymaria_p2_ap1.presentation.huacales.HuacalViewModel
 
 @Composable
 fun DeleteHuacalScreen(
