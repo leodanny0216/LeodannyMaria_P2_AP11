dc81a756cc21ae0a5a6fcc77eb9493fef3e93a68
leodanny0216 <leodannymaria2@gmail.com>
2025-10-01 22:54:23 -0400

    le agregue hilt al MainActivity.kt

--- app/src/main/java/edu/ucne/leodannymaria_p2_ap1/MainActivity.kt
+++ app/src/main/java/edu/ucne/leodannymaria_p2_ap1/MainActivity.kt
@@ -7,31 +7,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
-import edu.ucne.huacales.data.repository.EntradaHuacalRepository
-import edu.ucne.joserivera_ap2_p1.presentation.huacales.HuacalNavigation
-import edu.ucne.joserivera_ap2_p1.presentation.huacales.HuacalViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.leodannymaria_p2_ap1.ui.theme.LeodannyMaria_P2_AP1Theme
+import edu.ucne.joserivera_ap2_p1.presentation.huacales.HuacalNavigation
+import edu.ucne.leodannymaria_p2_ap1.presentation.huacales.HuacalViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // Crear DAO y repositorio
-        val database = HuacalDatabase.getDatabase(this)
-        val repository = EntradaHuacalRepository(database.huacalDao())
-
-        // Crear ViewModel manualmente
-        val viewModel = HuacalViewModel(repository)
-
         setContent {
             LeodannyMaria_P2_AP1Theme {
+                val navController = rememberNavController()
+                val viewModel: HuacalViewModel = hiltViewModel()
+
                 Surface(
-                    modifier = androidx.compose.ui.Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
                     HuacalNavigation(
                         navController = navController,
                         viewModel = viewModel
@@ -41,4 +39,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
